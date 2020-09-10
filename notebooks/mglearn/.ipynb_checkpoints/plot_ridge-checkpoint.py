398f2b4021e78be1c9fb45b177768f1d759fdfc4
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2020-09-10 09:28:17 +0200

    Linear models

--- notebooks/mglearn/.ipynb_checkpoints/plot_ridge-checkpoint.py
+++ notebooks/mglearn/.ipynb_checkpoints/plot_ridge-checkpoint.py
@@ -12,9 +12,9 @@ def plot_learning_curve(est, X, y):
         est, X, y, train_sizes=np.linspace(.1, 1, 20), cv=KFold(20, shuffle=True, random_state=1))
     estimator_name = est.__class__.__name__
     line = plt.plot(training_set_size, train_scores.mean(axis=1), '--',
-                    label="training " + estimator_name)
+                    label="training " + estimator_name, lw=2)
     plt.plot(training_set_size, test_scores.mean(axis=1), '-',
-             label="test " + estimator_name, c=line[0].get_color())
+             label="test " + estimator_name, c=line[0].get_color(), lw=2)
     plt.xlabel('Training set size')
     plt.ylabel('Score (R^2)')
     plt.ylim(0, 1.1)
